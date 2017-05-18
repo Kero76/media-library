5ca77b8830e1f7377424e40b71f058b440e61f31
Kero76 <nic.gille@gmail.com>
2017-05-18 14:33:05 +0200

    [Exception] - Added CartoonException.

---
+++ src/main/java/fr/nicolasgille/medialibrary/controllers/CartoonController.java
@@ -0,0 +1,7 @@
+package fr.nicolasgille.medialibrary.controllers;
+
+/**
+ * Created by Kero76 on 18/05/17.
+ */
+public class CartoonController {
+}
---
+++ src/main/java/fr/nicolasgille/medialibrary/exception/CartoonException.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of Media-Library.
+ *
+ * Media-Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Media-Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Media-Library. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.nicolasgille.medialibrary.exception;
+
+/**
+ * Exception send at the client when a problem occurred during CRUD process.
+ *
+ * @author Nicolas GILLE
+ * @since Media-Library 0.3
+ * @see fr.nicolasgille.medialibrary.controllers.CartoonController
+ * @version 1.0
+ */
+public class CartoonException extends Exception {
+
+    /**
+     * Message at display when the exception was throw.
+     *
+     * @since 1.0
+     */
+    private String message;
+
+    /**
+     * Constructor of the exception.
+     *
+     * @param message Message at display when the error was throw.
+     * @version 1.0
+     * @since 1.0
+     */
+    public CartoonException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    /**
+     * Message at display when the exception was throw.
+     *
+     * @return The message at display.
+     * @version 1.0
+     * @since 1.0
+     */
+    @Override
+    public String toString() {
+        return this.message;
+    }
+}
